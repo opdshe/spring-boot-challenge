9adc6c760e06afd06f8389e93e1ac6f04d15bfc1
opdshe <opdshe@naver.com>
2020-06-25 15:34:19 +0900

    feat : paging 기능 완성

--- src/main/java/com/springboot/challenge/domain/item/ItemRepository.java
+++ src/main/java/com/springboot/challenge/domain/item/ItemRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Page<Item> findAll(Pageable pageable);
--- src/main/resources/static/js/app/items.js
+++ src/main/resources/static/js/app/items.js
@@ -1,19 +1,33 @@
-function get_query(){
-    var url = document.location.href;
-    var qs = url.substring(url.indexOf('?') + 1).split('&');
-    for(var i = 0, result = {}; i < qs.length; i++){
-        qs[i] = qs[i].split('=');
-        result[qs[i][0]] = decodeURIComponent(qs[i][1]);
+let page ={
+    init : function(){
+        this.setPageUrl();
+        this.setActivePage();
+    },
+    setPageUrl :function() {
+        let parameters = this.getParameters();
+        $('.page_nation a').each(function (index, item) {
+            if (parameters['category'] === 'undefined' || parameters['category'] == null) {
+                $(item).attr('href', '?&page=' + (index + 1));
+            } else {
+                $(item).attr('href', '?category=' + parameters['category'] + '&page=' + (index + 1));
+            }
+            $(item).attr('id','page'+(index + 1))
+        });
+    },
+    getParameters : function () {
+        let url = document.location.href;
+        let qs = url.substring(url.indexOf('?') + 1).split('&');
+        for (var i = 0, result = {}; i < qs.length; i++) {
+            qs[i] = qs[i].split('=');
+            result[qs[i][0]] = decodeURIComponent(qs[i][1]);
+        }
+        return result;
+    },
+    setActivePage: function() {
+        let parameters = this.getParameters();
+        let currentPage = parameters['page'];
+        $('.page_nation').children('#page'+currentPage).attr('class','active');
     }
-    return result;
 }
-let parameters = get_query();
-console.log(parameters['category']);
-$('.page_num').each(function (index, item) {
-    if(parameters['category']==='undefined' || parameters['category']==null){
-        $(item).attr('href','?&page='+(index+1));
-    }
-    else {
-        $(item).attr('href','?category='+parameters['category']+'&page='+(index+1));
-    }
-});
+page.init();
+
