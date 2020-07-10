31f2784b7ffea79b617000cf1b3258fb1c369343
opdshe <opdshe@naver.com>
2020-07-10 22:18:00 +0900

    refactor :  UserController의 비지니스 로직 모두 UserService로 이동

--- src/main/java/com/springboot/challenge/service/UserService.java
+++ src/main/java/com/springboot/challenge/service/UserService.java
@@ -6,11 +6,15 @@ import com.springboot.challenge.domain.order.OrderRepository;
 import com.springboot.challenge.domain.order.Orders;
 import com.springboot.challenge.exceptions.MemberMismatchException;
 import com.springboot.challenge.web.dto.MemberRegisterRequestDto;
+import com.springboot.challenge.web.dto.OrderDtoForMyPage;
+import com.springboot.challenge.web.util.SessionManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,6 +25,16 @@ public class UserService {
     @Transactional
     public Long register(MemberRegisterRequestDto memberRegisterRequestDto) {
         return memberRepository.save(memberRegisterRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<OrderDtoForMyPage> getOrderDtosForMyPage(HttpSession httpSession){
+        String userId = SessionManager.getUserSessionAttribute(httpSession).getUsername();
+        Member member = this.findByUserId(userId);
+        List<Orders> ordersFindByMember = this.findAllByMember(member);
+        return ordersFindByMember.stream()
+                .map(OrderDtoForMyPage::new)
+                .collect(Collectors.toList());
     }
 
     @Transactional
--- src/main/java/com/springboot/challenge/web/UserController.java
+++ src/main/java/com/springboot/challenge/web/UserController.java
@@ -1,10 +1,6 @@
 package com.springboot.challenge.web;
 
-import com.springboot.challenge.domain.member.Member;
-import com.springboot.challenge.domain.order.Orders;
 import com.springboot.challenge.service.UserService;
-import com.springboot.challenge.web.dto.OrderDtoForMyPage;
-import com.springboot.challenge.web.util.SessionManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
@@ -34,13 +28,7 @@ public class UserController {
 
     @GetMapping("/mypage")
     public String mypage(HttpSession httpSession, Model model) {
-        String userId = SessionManager.getUserSessionAttribute(httpSession).getUsername();
-        Member member = userService.findByUserId(userId);
-        List<Orders> allOrdersFindByMember = userService.findAllByMember(member);
-        List<OrderDtoForMyPage> orderList = allOrdersFindByMember.stream()
-                .map(OrderDtoForMyPage::new)
-                .collect(Collectors.toList());
-        model.addAttribute("orderList", orderList);
+        model.addAttribute("orderList", userService.getOrderDtosForMyPage(httpSession));
         return "mypage";
     }
 }
